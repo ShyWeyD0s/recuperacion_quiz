8f66f9f6c242e3827e6718462367ead4a6d2c61e
ShyWeyD0s <sebastianchandesu62@gmail.com>
2026-04-21 15:46:06 -0500

    add: agencia, particular models

--- app/Models/Agencia.php
+++ app/Models/Agencia.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Agencia extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nombre', 'direccion', 'telefono', 'nombre_persona'];
+
+    public function habitaciones()
+    {
+        return $this->belongsToMany(Habitacion::class, 'reserva_agencia')
+            ->withPivot('fecha_ini', 'fecha_fin')
+            ->withTimestamps();
+    }
 }
--- app/Models/Particular.php
+++ app/Models/Particular.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Particular extends Model
 {
     use HasFactory;
+
+    protected $table = 'particulares';
+
+    protected $fillable = ['nombre', 'direccion', 'telefono'];
+
+    public function habitaciones()
+    {
+        return $this->belongsToMany(Habitacion::class, 'reserva_particular')
+            ->withPivot('fecha_ini', 'fecha_fin')
+            ->withTimestamps();
+    }
 }
